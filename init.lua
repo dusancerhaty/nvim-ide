ffa6261a02aad334adf3fc383c550e62d10e70a4
Dusan Cerhaty <cerhaty@eset.sk>
2023-07-04 15:10:20 +0200

    lspconfig: use default_capabilities() instead of deprecated update_capabilities()

--- init.lua
+++ init.lua
@@ -321,7 +321,7 @@ cmp.setup.cmdline(':', {
 })
 
 -- Setup lspconfig.
-local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
+local capabilities = require('cmp_nvim_lsp').default_capabilities(vim.lsp.protocol.make_client_capabilities())
 
 -- Mappings.
 -- See `:help vim.diagnostic.*` for documentation on any of the below functions
