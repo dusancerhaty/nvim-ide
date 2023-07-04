167f870954175c9fe0e75118fdd02fdac8f63bc7
Dusan Cerhaty <cerhaty@eset.sk>
2023-07-04 15:13:54 +0200

    makeprg: remove colored scons output

--- init.lua
+++ init.lua
@@ -386,7 +386,7 @@ for _, lsp in ipairs(servers) do
 end
 
 -- Change makeprg
-vim.opt.makeprg = 'scons --verbose-level=none'
+vim.opt.makeprg = 'scons --verbose-level=none --with=compiler_color=off'
 vim.opt.shellpipe = '2>&1| tee .build.log'
 
 -- Quickfix
