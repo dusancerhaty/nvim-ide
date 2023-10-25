47b5cfd8826db1483de9820427937544cefba5c1
Dusan Cerhaty <cerhaty@eset.sk>
2023-10-25 11:20:51 +0200

    Change colorscheme to Tokyonight

--- init.lua
+++ init.lua
@@ -78,6 +78,7 @@ require('packer').startup(function()
   use { 'folke/trouble.nvim', requires = { 'folke/lsp-colors.nvim' } }
   use { 'folke/todo-comments.nvim', requires = { 'folke/trouble.nvim', 'nvim-tree/nvim-web-devicons' } }
   use { 'nvim-tree/nvim-tree.lua', requires = { 'nvim-tree/nvim-web-devicons' } }
+  use 'folke/tokyonight.nvim'
 end)
 
 require('nvim-web-devicons').setup {
@@ -119,7 +120,7 @@ vim.wo.signcolumn = 'auto:1-9'
 --Set colorscheme
 vim.o.termguicolors = true
 vim.o.background = "dark"
-vim.cmd [[colorscheme gruvbox]]
+vim.cmd [[colorscheme tokyonight-night]]
 
 -- Set completeopt to have a better completion experience
 vim.o.completeopt = 'menuone,noselect'
@@ -127,8 +128,8 @@ vim.o.completeopt = 'menuone,noselect'
 --Set statusbar
 require('lualine').setup {
   options = {
-    icons_enabled = false,
-    theme = 'gruvbox',
+    icons_enabled = true,
+    theme = 'tokyonight',
     component_separators = '|',
     section_separators = '',
   },
@@ -483,5 +484,42 @@ vim.api.nvim_set_keymap('n', '<leader>tc', '<cmd>NvimTreeClose<CR>', { noremap =
 vim.api.nvim_set_keymap('n', '<leader>tt', '<cmd>NvimTreeToggle<CR>', { noremap = true, silent = true })
 vim.api.nvim_set_keymap('n', '<leader>tf', '<cmd>NvimTreeFocus<CR>', { noremap = true, silent = true })
 vim.api.nvim_set_keymap('n', '<leader>tr', '<cmd>NvimTreeRefresh<CR>', { noremap = true, silent = true })
+
+-- Tokyonight colorscheme
+require("tokyonight").setup({
+  -- your configuration comes here
+  -- or leave it empty to use the default settings
+  style = "night", -- The theme comes in three styles, `storm`, `moon`, a darker variant `night` and `day`
+  light_style = "day", -- The theme is used when the background is set to light
+  transparent = false, -- Enable this to disable setting the background color
+  terminal_colors = true, -- Configure the colors used when opening a `:terminal` in [Neovim](https://github.com/neovim/neovim)
+  styles = {
+    -- Style to be applied to different syntax groups
+    -- Value is any valid attr-list value for `:help nvim_set_hl`
+    comments = { italic = false },
+    keywords = { italic = false },
+    functions = {},
+    variables = {},
+    -- Background styles. Can be "dark", "transparent" or "normal"
+    sidebars = "dark", -- style for sidebars, see below
+    floats = "dark", -- style for floating windows
+  },
+  sidebars = { "qf", "help" }, -- Set a darker background on sidebar-like windows. For example: `["qf", "vista_kind", "terminal", "packer"]`
+  day_brightness = 0.3, -- Adjusts the brightness of the colors of the **Day** style. Number between 0 and 1, from dull to vibrant colors
+  hide_inactive_statusline = false, -- Enabling this option, will hide inactive statuslines and replace them with a thin border instead. Should work with the standard **StatusLine** and **LuaLine**.
+  dim_inactive = true, -- dims inactive windows
+  lualine_bold = false, -- When `true`, section headers in the lualine theme will be bold
+
+  --- You can override specific color groups to use other groups or a hex color
+  --- function will be called with a ColorScheme table
+  ---@param colors ColorScheme
+  on_colors = function(colors) end,
+
+  --- You can override specific highlights to use other groups or a hex color
+  --- function will be called with a Highlights and ColorScheme table
+  ---@param highlights Highlights
+  ---@param colors ColorScheme
+  on_highlights = function(highlights, colors) end,
+})
 
 -- vim: ts=2 sts=2 sw=2 et
