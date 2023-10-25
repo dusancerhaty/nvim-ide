a7183bd92020647cdc7ec6f3ce63baedd942745d
Dusan Cerhaty <cerhaty@eset.sk>
2023-10-25 15:07:43 +0200

    Indent blankline is working now

--- init.lua
+++ init.lua
@@ -155,11 +155,12 @@ vim.cmd [[
   augroup end
 ]]
 
---Map blankline
-vim.g.indent_blankline_char = '┊'
-vim.g.indent_blankline_filetype_exclude = { 'help', 'packer' }
-vim.g.indent_blankline_buftype_exclude = { 'terminal', 'nofile' }
-vim.g.indent_blankline_show_trailing_blankline_indent = false
+-- Indent blankline
+require("ibl").setup {
+  indent = {
+    char = '┊',
+  },
+}
 
 -- Gitsigns
 require('gitsigns').setup {
